3c685961bf28cc45a40017a3605c7e5096af85a1
victor <victor.rivero1204@gmail.com>
2025-01-25 17:39:18 -0400

    hack_flask

--- app.py
+++ app.py
@@ -6,8 +6,45 @@ app = Flask(__name__)
 CORS(app)
 
 
+@app.route("/user", methods=['GET', 'POST', 'DELETE', 'PUT'])
+def users():
+    return jsonify({'payload':'success'})
 
 
+@app.route("/api/v1/users", methods=['GET'])
+def v1_users():
+    request.method == 'GET'
+    return jsonify({'payload': []})
 
-if __name__ == "__main__":
+
+@app.route("/api/v1/users", methods=['POST'])
+def v1_create_users():
+    email = request.args.get('email')
+    nombre = request.args.get('nombre')
+    
+    return jsonify ({'payload':{'email': email, 'nombre': nombre,}})
+
+    
+@app.route("/api/v1/user/add", methods=['POST'])
+def v1_add_user():
+    nombre = request.form.get('nombre')
+    email = request.form.get('email')
+    user_id = request.form.get('id')
+    
+    return jsonify ({'payload':{'email': email, 'nombre': nombre, 'id': user_id}})
+
+
+@app.route("/api/v1/user/create", methods=['POST'])
+def v1_create_user():
+    
+    data = request.get_json()
+    
+    nombre = data.get('nombre')
+    email = data.get('email')
+    user_id = data.get('id')
+    
+    return jsonify ({'payload':{'email': email, 'nombre': nombre, 'id': user_id}})
+
+
+if __name__ == '__main__':
     app.run(debug=True)
